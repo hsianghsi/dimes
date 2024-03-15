7960a99b205cc6c372c7b3eb2830ea4970ddb678
bossbossleu <bossbossleu@gmail.com>
2024-03-16 00:09:16 +0800

    close pano = close x

--- script.js
+++ script.js
@@ -410,10 +410,10 @@ function showPanorama(panoramaImageUrl) {
 
   // Handle the close button click event
   const closeButton = document.createElement('button');
-  closeButton.innerHTML = 'Close Pano';
+  closeButton.innerHTML = 'Close X';
   closeButton.style.position = 'absolute';
-  closeButton.style.top = '10px';
-  closeButton.style.right = '10px';
+  closeButton.style.top = '15px';
+  closeButton.style.right = '15px';
   closeButton.style.zIndex = '1002'; // Set z-index to be on top of panorama container
   closeButton.style.cursor = 'pointer';
   closeButton.id = 'closePanorama';
